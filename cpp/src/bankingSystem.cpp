79a0d7102711442d643e3330014052ac0c1e60fb
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-05-11 16:27:29 +0330

    Final Version

--- cpp/src/bankingSystem.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/bankingSystem.h"
-
-double BankingSystem::convert(double amount, const Currency& from_currency,
-    const Currency& to_currency) const
-{
-    return ((amount * to_currency.getExchangeRate()) / from_currency.getExchangeRate());
-}
